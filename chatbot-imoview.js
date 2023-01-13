9265268212280c74c5ac19efce30b69306cbc361
maranatatecnologia <developer@maranatatecnologia.com.br>
2023-01-13 13:14:50 -0300

    Aplicando MD5 na senha e finalizando envio do anexo

--- chatbot-imoview.js
+++ chatbot-imoview.js
@@ -124,29 +124,12 @@ var IMOVIEW = {
         var valoresAnterioresObjeto = new Array();
         // valoresAnterioresObjeto = valoresAnteriores;
         
-        if (tipo == "upload") {
-             input = document.querySelector('#' + id+'');
-            const file = input.files[0];
-            const reader = new FileReader();
-            reader.onload = function () {
-                const base64 = btoa(reader.result);
-                console.log(base64);
-            };
-            reader.readAsBinaryString(file);
-
-            valoresAnteriores = JSON.parse(decodeURIComponent(valoresAnteriores));
-            Object.assign(valoresAnterioresObjeto, valoresAnteriores);
-            
-            var novoValor = {
-                "constante": input.dataset.constante,
-                "label": input.dataset.label,
-                "valor": base64
-            }
-        }
+  
         if (seletorInput != "") {
              var itemConteudo = document.querySelector('#'+seletorInput);
         }     
 
+      
          if (IMOVIEW.Maximado == false) {
             document.querySelector('.botaoMaximar').addEventListener("click", IMOVIEW.MaximarJanelaChat);  
         }
@@ -162,6 +145,19 @@ var IMOVIEW = {
             valoresAnteriores = JSON.parse(decodeURIComponent(valoresAnteriores))
             Object.assign(valoresAnterioresObjeto, valoresAnteriores)
         }
+
+        if (tipo == "upload") {
+            base64 = itemConteudo.dataset.base64;
+            nomeArquivo = itemConteudo.dataset.nome;
+
+            var novoValor = {
+                "constante": input.dataset.constante,
+                "label": nomeArquivo,
+                "valor": base64
+            }
+            valoresAnterioresObjeto.push(novoValor)
+        }
+
         
         if(card == true){
             inputDados = document.querySelector('#'+seletorInput);
@@ -183,10 +179,13 @@ var IMOVIEW = {
             IMOVIEW.RetonarSelecionado(label);
         }
 
-        if(seletorInput != "" && botao == false && card == false){
+        if(seletorInput != "" && botao == false && card == false && tipo != "upload"){
             inputDados = document.querySelector('#'+seletorInput);
             valorInput = (ehSelect ? inputDados.dataset.value : inputDados.value);
             label = (ehSelect ? inputDados.dataset.label : inputDados.placeholder);
+            if(tipo == 'password'){
+                valorInput = IMOVIEW.MD5(valorInput);
+            }
 
             if (valorInput == "") {
                  IMOVIEW.RetornandoAlert('danger', 'Informação obrigatória')
@@ -442,7 +441,7 @@ var IMOVIEW = {
                             case 'upload': 
                                 propriedades = "";
                                
-                                htmlInput += '<input onchange="IMOVIEW.FileBase64(\''+idInput+'\')" type="file" class="limparChat input-chat"  id="' + idInput + '" value="' + menu.valorPadrao + '" ' + propriedades + ' data-constante="' + menu.constante + '" type="text"  autocomplete="name" placeholder="' + menu.nome + '" list="" style="border-color: rgb(76, 175, 80);"><div class="imoview-submit" onclick=\"IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + idInput + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\')\"></div>' + htmlAjuda;                        
+                                htmlInput += '<input id="'+IdBotaoOpcoes+'" onchange=\"IMOVIEW.FileToBase64Input(\'' + IdBotaoOpcoes + '\')\" type="file" class="limparChat input-chat"  value="" ' + propriedades + ' data-base64="" data-nome="" data-constante="' + menu.constante + '" autocomplete="name" placeholder="' + menu.nome + '" list="" style="border-color: rgb(76, 175, 80);"><div class="imoview-submit" onclick=\"IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + IdBotaoOpcoes + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\', false, false, 1, true, \'upload\')\"></div>' + htmlAjuda;                        
                                 document.querySelector('.imoview-chatbot .imoview-ctx>div>.imoview-container').style.paddingBottom = '60px';
                                 document.querySelector('.imoview-input').style.display = 'block';
                                 break
@@ -452,7 +451,7 @@ var IMOVIEW = {
                                 document.querySelector('.imoview-input').style.display = 'block';
                                 break;
                             case 'password':
-                                htmlInput += '<input  onkeypress="javascript: if(event.keyCode == 13) IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + idInput + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\');" class="limparChat input-chat" id="' + idInput + '" value="" data-constante="' + menu.constante + '" type="password" autocomplete="name" onblur="clearTimeout()" placeholder="' + menu.nome + '" list="" style="border-color: rgb(76, 175, 80);"><div class="imoview-submit" onclick=\"IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + idInput + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\')\"></div>' + htmlAjuda;
+                                htmlInput += '<input  onkeypress="javascript: if(event.keyCode == 13) IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + idInput + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\');" class="limparChat input-chat" id="' + idInput + '" value="" data-constante="' + menu.constante + '" type="password" autocomplete="name" onblur="clearTimeout()" placeholder="' + menu.nome + '" list="" style="border-color: rgb(76, 175, 80);"><div class="imoview-submit" onclick=\"IMOVIEW.CarregarConversa(' + constanteAtual + ', \'' + idInput + '\' ,\'' + (valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))) + '\',false, false, 1, false, \'password\')\"></div>' + htmlAjuda;
                                 document.querySelector('.imoview-chatbot .imoview-ctx>div>.imoview-container').style.paddingBottom = '60px';
                                 document.querySelector('.imoview-input').style.display = 'block';
                                 break;
@@ -524,6 +523,10 @@ var IMOVIEW = {
             loadingPag.remove();
         });
     },
+    MD5: function (value) {
+       var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
+       return MD5(value);
+    },
     RetonarSelecionado: function (texto = "", selecionadoOuResposta = "Selecionado") {        
         htmlSelecionado = '<div class="imoview-group imoview-right"><div class="imoview-container"><div class="imoview-msg"><div class="imoview-bubble" style="background: #4c4c4c;color: white !important;"><div><div><p style="color:white !important;">'+selecionadoOuResposta+': '+texto+'</p></div><div class="imoview-clear"></div></div></div></div></div><div class="imoview-clear"></div></div>';
         botoesNaTela = document.querySelectorAll('.removerAposClickBotao');
@@ -555,6 +558,43 @@ var IMOVIEW = {
         };
         reader.readAsBinaryString(file);
 
+    },
+    FileToBase64: function (constanteAtual = 0, seletorInput = "", valoresAnteriores = "", botao = false, card = false, pagina = 1, ehSelect = false, tipo = "") {
+            input = document.querySelector('#' + seletorInput+'');
+            const file = input.files[0];
+            const reader = new FileReader();
+            reader.onload = function () {
+                const base64 = btoa(reader.result);
+                console.log(base64);
+                var valoresAnterioresObjeto = new Array();
+                valoresAnteriores = JSON.parse(decodeURIComponent(valoresAnteriores));
+                Object.assign(valoresAnterioresObjeto, valoresAnteriores);
+
+                var novoValor = {
+                    "constante": input.dataset.constante,
+                    "label": input.files[0].name,
+                    "valor": base64
+                }
+                
+                valoresAnteriores.push(novoValor)
+
+                IMOVIEW.CarregarConversa(constanteAtual, seletorInput,(valoresAnteriores == "" ? 0 : encodeURIComponent(JSON.stringify(valoresAnteriores))), false, false, 1, true, 'upload')
+            
+              
+            };
+            reader.readAsBinaryString(file);
+    },
+    FileToBase64Input: function (seletorInput) {
+        input = document.querySelector('#' + seletorInput+'');
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.onload = function () {
+            const base64 = btoa(reader.result);
+            console.log(base64);            
+            input.dataset.base64 = base64;
+            input.dataset.nome = input.files[0].name;
+        };
+        reader.readAsBinaryString(file);
     },
     RetornandoAlert: function (tipo, mensagem, retornarBotaoVoltarPrincipal = false) {
        htmlAlert = 
